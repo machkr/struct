456f9e906f497af674718fdc874d07724f437626
Bruthor <Sterling2@mail.usf.edu>
2016-05-26 19:30:32 -0400

    Initial Commit
    Added the described members, methods, and constructors.

--- project1/SingleNode.h
+++ project1/SingleNode.h
@@ -2,10 +2,29 @@
 #include <iostream>
 using namespace std;
 
-class SingleNode
+template <class type> class SingleNode
 {
 public:
-	SingleNode();
-	~SingleNode();
+	type data;
+	SingleNode * next;
+	SingleNode() : data(0), next(nullptr)	//Default Constructor
+	{
+		//Nothing? 
+	}
+	SingleNode(Type const & data, SingleNode * next)	 //Constructor
+	{
+		this->data = data;
+		this->next = next;
+	}
+
+	Type getData() const{		//Data accessor
+		return this->data;
+	}
+
+	SingleNode *getNext() const {		//Next accessor
+		return this->next;
+	}
+
+	~SingleNode();		//Default Destructor
 };
 
