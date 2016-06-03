09d70afb21851c137d933b95d030791749efbe80
Matthew <kramerm@mail.usf.edu>
2016-06-02 22:00:04 -0400

    Small changes to SingleNode.h, DoubleNode.h variables/methods created

--- project1/DoubleNode.h
+++ project1/DoubleNode.h
@@ -2,10 +2,38 @@
 #include <iostream>
 using namespace std;
 
-class DoubleNode
+template <class Type> class DoubleNode
 {
-public:
-	DoubleNode();
-	~DoubleNode();
-};
+private:
+	Type data;
+	DoubleNode *previous;
+	DoubleNode *next;
+	friend class DoublyLinkedList;
 
+public:
+	DoubleNode() : data(0), previous(nullptr), next(nullptr) {}
+
+	DoubleNode(Type const &data, DoubleNode *previous, DoubleNode *next)
+	{
+		this->data = data;
+		this->previous = previous;
+		this->next = next;
+	}
+
+	Type getData() const
+	{
+		return this->data;
+	}
+
+	DoubleNode *getNext() const
+	{
+		return this->next;
+	}
+
+	DoubleNode *getPrevious() const
+	{
+		return this->previous;
+	}
+
+	~DoubleNode() {};
+};
