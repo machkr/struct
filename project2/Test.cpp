2ebd871747c8bc9fef577c9a5f2d0891903c8110
Bruthor <sterling2@mail.usf.edu>
2016-06-16 13:25:52 -0400

    Queue methods update. All should work except for erase.

--- project2/Test.cpp
+++ project2/Test.cpp
@@ -3,30 +3,51 @@
 
 int main()
 {
-	/*DynQueue<int> x = DynQueue<int>(5);*/
-	DynStack<int> y = DynStack<int>(2);
+	DynQueue<int> x = DynQueue<int>(5);
+	//DynStack<int> y = DynStack<int>(2);
 
-	//x.enqueue(5);
-	//x.enqueue(4);
-	//x.enqueue(3);
-	//x.enqueue(2);
-	//x.enqueue(1);
-	//x.enqueue(0);
+	x.enqueue(5);
+	x.enqueue(4);
+	x.enqueue(3);
+	x.enqueue(2);
+	x.enqueue(1);
+	x.enqueue(0);
+	x.enqueue(-1);
+	x.display();
+
+	x.dequeue();
+	x.display();
+	x.dequeue();
+	x.display();
+	x.dequeue();
+	x.display();
+
+	x.enqueue(100);
+	x.display();
+
+	x.enqueue(100);
+	x.enqueue(100);
+	x.enqueue(100);
+	x.enqueue(7);
+	x.display();
+
+	//cout << "Let's see if we can delete all FOUR 100's!!" << endl;
+	//x.erase(100);
 	//x.display();
 
-	y.push(1);
-	y.push(0);
-	y.push(1);
-	y.push(0);
-	y.push(1);
-	y.push(0);
-	y.display();
+	//y.push(1);
+	//y.push(0);
+	//y.push(1);
+	//y.push(0);
+	//y.push(1);
+	//y.push(0);
+	//y.display();
 
-	y.erase(1);
-	y.display();
+	//y.erase(1);
+	//y.display();
 
-	y.clear();
-	y.display();
+	//y.clear();
+	//y.display();
 
 	cin.get();
 	return 0;
