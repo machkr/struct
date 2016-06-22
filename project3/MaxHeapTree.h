878afb2c5692484d038610f1cab1e3aedee9a494
machkr <matthewcharleskramer@gmail.com>
2016-06-22 14:06:33 -0400

    Addition of MaxHeapTree class

---
+++ project3/MaxHeapTree.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+template <class type>
+
+class MaxHeapTree
+{
+private:
+
+
+public:
+	//Constructor
+	MaxHeapTree(): 
+	{
+
+	}
+
+	//Destructor
+	~MaxHeapTree() :
+	{
+
+	}
+
+	//Accessors
+	getMax()
+	{
+
+	}
+
+	getSize()
+	{
+
+	}
+
+	getHeight()
+	{
+
+	}
+
+	empty()
+	{
+
+	}
+
+	leaves()
+	{
+
+	}
+
+	//Mutators
+	void buildTree()
+	{
+
+	}
+
+	void clear()
+	{
+
+	}
+
+	void insert(int key, type data)
+	{
+
+	}
+
+	void delMax()
+	{
+
+	}
+};
