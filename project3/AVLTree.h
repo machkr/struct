585e97826331119d886349b0a397808979dbecc4
Bruthor <Sterling2@mail.usf.edu>
2016-07-04 12:29:37 -0400

    Got rid of custom Max function due to Linker error, added algorithm library to AVL Tree. Should compile now

--- project3/AVLTree.h
+++ project3/AVLTree.h
@@ -2,21 +2,7 @@
 #include "TreeNode.h"
 #include <fstream>
 #include <string>
-
-//int max(int num1, int num2)
-//{
-//	if (num1 >= num2)
-//	{
-//		return num1;
-//	}
-//
-//	if (num2 >= num1)
-//	{
-//		return num2;
-//	}
-//
-//	else return 0;
-//}
+#include <algorithm>
 
 using namespace std;
 
